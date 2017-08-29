5ac3d3ec6bed362412fb7eb241b746a91f43fadf
Marina Shcherbak <shcherbak.marina92@gmail.com>
2017-08-29 19:38:16 +0300

    bag

--- final/final/js/item.js
+++ final/final/js/item.js
@@ -2,9 +2,31 @@ var addToBagButton = document.getElementsByClassName('addToBag')[0];
 
 
 addToBagButton.addEventListener('click', addToBag);
-// console.log(addToBag);
 
 function addToBag() {
-  var parent = this.parentElement;
-  console.log(parent);
+  var bag = getBagContent() || {};
+  var item = this.parentElement.parentElement.parentElement;
+  var itemName = item.querySelector('.name').innerHTML;
+  var itemSize = item.querySelector('input[name="size"]:checked').value;
+  var itemColor = item.querySelector('input[name="color"]:checked').value;
+  var itemId = itemName + '/' + itemSize + '/' + itemColor;
+
+
+  if (bag.hasOwnProperty(itemId) && bag[itemId][2] == itemSize && bag[itemId][3] == itemColor) {
+  	bag[itemId][4].amount += 1;
+  } else {
+    var itemPrice = item.querySelector('.price').innerHTML.substring(1);
+    var itemAmount = 1;
+    bag[itemId] = [itemName, itemPrice, itemSize, itemColor, {amount: itemAmount}];
+  }
+
+  updateBagContent(bag);
+}
+
+function getBagContent() {
+  return JSON.parse(localStorage.getItem('bag'));
+}
+
+function updateBagContent(bagContent) {
+  localStorage.setItem('bag', JSON.stringify(bagContent));
 }
